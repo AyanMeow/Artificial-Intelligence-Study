7e6205bd6408f54222c13c85560858b53cc67e7b
AyanMeow <781030835@qq.com>
2023-10-18 00:01:57 +0800

    11

--- Text classification/RAE/RAE.py
+++ Text classification/RAE/RAE.py
@@ -1,7 +1,8 @@
 import torch.nn as nn
+import torch.nn.functional as F
 import torch 
 import numpy as np
-
+from Tree import RTree
 
 class RAE(nn.Module):
     """docstring for RAE.
@@ -11,29 +12,45 @@ class RAE(nn.Module):
                  input_dim,
                  K,
                  ):
-     super(RAE, self).__init__()
-     self.device=device
-     self.encoder=nn.Sequential(
-         nn.Linear(256,200).to(device),
-         nn.Linear(200,128).to(device)
-     )
-     self.decoder=nn.Sequential(
-         nn.Linear(128,200).to(device),
-         nn.Linear(200,256).to(device)
-     )
+        super(RAE, self).__init__()
+        self.device=device
+        self.encoder=nn.Sequential(
+            nn.Linear(256,200).to(device),
+            nn.Linear(200,128).to(device)
+        )
+        self.decoder=nn.Sequential(
+            nn.Linear(128,200).to(device),
+            nn.Linear(200,256).to(device)
+        )
+        self.bitree=None
      
-     def forward(self,inputs):
+    def forward(self,inputs):
         '''slidewindow from right
         input:(N,128)
         '''
         inputs=inputs.to(self.device)
-        seq_len=inputs.size(0)
-        while seq_len>1:
+        seq_len,m=inputs.size()
+        nodelist=[]
+        for i in range(0,seq_len,1):
+            n=RTree(x=inputs[i])
+            nodelist.append(n)
+        while len(nodelist)>1:
             father_temp=[]
             recon_loss_temp=[]
-            for i in range(1,seq_len,1):
-                inputs
-            
+            for i in range(len(nodelist)-1,0,-1):
+                #计算父节点和重建损失
+                temp=torch.concat([nodelist[i].x,nodelist[i-1].x])
+                yi=self.encoder(temp)
+                xx=self.decoder(yi)
+                loss1=F.cross_entropy(nodelist[i].x,xx[0:m])
+                loss2=F.cross_entropy(nodelist[i-1].x,xx[m:-1])
+                #计算加权loss和
+                nodelist[i].cal_leaves()
+                nodelist[i-1].cal_leaves()
+                n1=nodelist[i].leave_count
+                n2=nodelist[i-1].leave_count
+                loss=n1/(n1+n2)*loss1+n2/(n1+n2)*loss2
+                
             
 
     
--- Text classification/RAE/tree.py
+++ Text classification/RAE/tree.py
@@ -1,1 +1,34 @@
-import numpy as np
+
+
+class RTree(object):
+    """docstring for RTree."""
+    def __init__(self,
+                 x=-1,
+                 K=-1,#softmax class
+                 loss=-1,
+                 lc=None,
+                 rc=None,):
+        super(RTree, self).__init__()
+        self.x=x
+        self.K=K
+        self.loss=loss
+        self.lc=lc
+        self.rc=rc
+        self.leave_count=0
+        
+    def cal_leaves(self,root=-1):
+        if root == -1:
+            self.leave_count=0
+            self.cal_leaves(self.lc)
+            self.cal_leaves(self.rc)
+        else:
+            if root == None:
+                return
+            if root.lc == None and root.rc == None:
+                self.leave_count+=1
+            self.cal_leaves(root.lc)
+            self.cal_leaves(root.rc)
+            
+        
+
+    
